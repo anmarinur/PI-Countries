29f761cfe47dc6a59a48a9fb62f74877d2692f33
anmarinur <andersonmarindev@gmail.com>
2022-09-03 09:13:55 -0500

    Add Country model

--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -3,10 +3,47 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('country', {
+  sequelize.define('Country', {
+    id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      primaryKey: true,
+      validate: {
+        isAlpha: true,
+        longId (value) {
+          if (value.length > 3) {
+            throw new Error('Debe tener menos de tres caracteres');
+          }
+        }
+      }
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    flagImg: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    continent: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    capital: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    subregion: {
+      type: DataTypes.STRING,
+      defaultValue: ' '
+    },
+    area: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0
+    },
+    population: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0
+    }
   });
 };
