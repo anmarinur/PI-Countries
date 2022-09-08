16752e4ccb99576a97b4891ca1703b84c32a734a
anmarinur <andersonmarindev@gmail.com>
2022-09-07 20:22:11 -0500

    Bring and select data from the api

--- api/src/controllers/CCountries.js
+++ api/src/controllers/CCountries.js
@@ -1,11 +1,24 @@
 const axios = require('axios');
 const { Country, Activity, CountryActivity } = require('../db');
 
-async function getCountry (req, res) {
+const getCountry = async (req, res) => {
 
   try {
-    console.log('flag')
-    let countries = (await axios.get('https://restcountries.com/v3/all')).data;
+    let countries = (await axios.get('https://restcountries.com/v3/all'));
+    console.log('start info')
+    countries = countries.data.map(p => {
+      console.log(p.capital)
+      return {
+          id: p.cca3,
+          name: p.name.common,
+          flagImg: p.flags[1],
+          continent: p.continents[0],
+          capital: p.capital,
+          subregion: p.subregion,
+          area: p.area,
+          population: p.population
+      };
+    });
     res.json(countries);
   } catch (error) {
     res.send(error)
