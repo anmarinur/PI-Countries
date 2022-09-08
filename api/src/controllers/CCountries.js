3b75c7bd580d83b91c5cd2d55702f150f4ccc7f1
anmarinur <andersonmarindev@gmail.com>
2022-09-08 15:19:27 -0500

    C:/Program Files/Git/countries end point complete

--- api/src/controllers/CCountries.js
+++ api/src/controllers/CCountries.js
@@ -5,29 +5,32 @@ const getCountry = async (req, res) => {
 
   try {
     let infoApi = await axios.get('https://restcountries.com/v3/all');
-    let infoData = await infoApi.data.map(p => {
-      console.log(p.capital)
+    let infoData = await infoApi.data.map(p => { 
       return {
           id: p.cca3,
           name: p.name.common,
           flagImg: p.flags[1],
           continent: p.continents[0],
-          capital: p.capital,
+          capital: !p.capital ? 'no tiene' : p.capital[0],
           subregion: p.subregion,
           area: p.area,
           population: p.population
       };
     });
-    res.json(infoData);
+    await Country.bulkCreate(infoData);
+    const result = await Country.findAll({
+      attributes: ['flagImg', 'name', 'continent']
+    });
+    res.json(result);
   } catch (error) {
-    res.send(error)
+    res.send(error);
   }
 }
 
 const getCountryById = async function (req, res) {
   const {idPais} = req.params;
-  console.log(idPais)
- 
+  console.log(idPais);
+  
 }
 
 module.exports = {
