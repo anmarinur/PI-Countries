198716a729a1efea9329c95f98b751d9cef69a29
anmarinur <andersonmarindev@gmail.com>
2022-09-09 12:17:12 -0500

    Start on CActivity controller

--- api/src/controllers/CActivity.js
+++ api/src/controllers/CActivity.js
@@ -2,6 +2,20 @@
 const { Country, Activity, CountryActivity } = require('../db');
 
 const postActivity = async function(res, req) {
+  const { name, difficult, duration, season } = req.body;
+
+  try {
+    if (name) {
+      const activity = await Activity.create({
+        name, difficult, duration, season
+      });
+      res.json(activity);
+    } else {
+      res.json('Datos incompletos');
+    }
+  } catch (error) {
+    console.log('Error: ', error);
+  }
 
 }
 
