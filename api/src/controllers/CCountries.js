4d864b6239f66b467ac0b45f3e4c7df3d48b8d32
anmarinur <andersonmarindev@gmail.com>
2022-09-08 21:14:28 -0500

    Add /countries?name end point

--- api/src/controllers/CCountries.js
+++ api/src/controllers/CCountries.js
@@ -1,36 +1,59 @@
 const axios = require('axios');
+const { Op } = require('sequelize');
 const { Country, Activity, CountryActivity } = require('../db');
 
 const getCountry = async (req, res) => {
-
-  try {
-    let infoApi = await axios.get('https://restcountries.com/v3/all');
-    let infoData = await infoApi.data.map(p => { 
-      return {
-          id: p.cca3,
-          name: p.name.common,
-          flagImg: p.flags[1],
-          continent: p.continents[0],
-          capital: !p.capital ? 'no tiene' : p.capital[0],
-          subregion: p.subregion,
-          area: p.area,
-          population: p.population
-      };
-    });
-    await Country.bulkCreate(infoData);
-    const allCountriesFront = await Country.findAll({
-      attributes: ['flagImg', 'name', 'continent']
-    });
-    res.json(allCountriesFront);
-
-  } catch (error) {
-    res.send(error);
+  const { name } = req.query;
+  if (!name) {
+    try {
+      let infoApi = await axios.get('https://restcountries.com/v3/all');
+      let infoData = await infoApi.data.map(p => { 
+        return {
+            id: p.cca3,
+            name: p.name.common,
+            flagImg: p.flags[1],
+            continent: p.continents[0],
+            capital: !p.capital ? 'No tiene' : p.capital[0],
+            subregion: p.subregion,
+            area: p.area,
+            population: p.population
+        };
+      });
+      await Country.bulkCreate(infoData);
+      const allCountriesFront = await Country.findAll({
+        attributes: ['flagImg', 'name', 'continent']
+      });
+      res.json(allCountriesFront);
+  
+    } catch (error) {
+      res.send(error);
+    }
+  } else {
+    try {
+      const countryByName = await Country.findAll({
+        where: {
+          name: {
+            [Op.iLike] : `%${name}%`
+          }
+        },
+        attributes: {
+          exclude: ['createdAt', 'updatedAt']
+        }
+      })
+      if (countryByName.length !== 0) {
+        res.json(countryByName);
+      } else {
+        res.json('País no encontrado')
+      }
+      
+    } catch (error) {
+      console.log(error)
+    }
   }
 }
 
 const getCountryById = async function (req, res) {
   const {idPais} = req.params;
-  // console.log(idPais);
 
   try {
     const infoCountry = await Country.findAll({
@@ -41,7 +64,6 @@ const getCountryById = async function (req, res) {
         exclude: ['createdAt', 'updatedAt'] 
       }
     })
-    // console.log(infoCountry)
     if (infoCountry.length === 0) {
       res.json('País no encontrado')
     }
