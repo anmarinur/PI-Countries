94cb61f1821758f8382f7401b211329f4909dffb
anmarinur <andersonmarindev@gmail.com>
2022-09-12 22:02:58 -0500

    Work on controller activities

--- api/src/controllers/CActivity.js
+++ api/src/controllers/CActivity.js
@@ -1,14 +1,15 @@
 // const axios = require('axios');
 const { Country, Activity, CountryActivity } = require('../db');
 
-const postActivity = async function(res, req, next) {
-  const { name, difficulty, duration, season } = req.body;
+const postActivity = async function(req, res, next) {
+  const { name, difficulty, duration, season, countries } = req.body;
 
   try {
     if (name) {
       const activity = await Activity.create({
         name, difficulty, duration, season
       });
+      Activity.addCountry(countries);
       res.json(activity);
     } else {
       res.json('Datos incompletos');
