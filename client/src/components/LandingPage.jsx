f31db44ce71eeebe0c8d57b4b2aabae24e77a41b
anmarinur <andersonmarindev@gmail.com>
2022-09-25 15:53:40 -0500

    Add new style to landingpage

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -2,18 +2,22 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import style from './LandingPage.module.css';
 import image from '../assets/background.jpg'
+import video from '../assets/video_background.mp4'
 
 export default function LandingPage (){
   return (
     <div className={style.component}>
-      <img className={style.image} src={image} alt='background'/>
-      <div className={style.headers}>
-        <h1 className={style.h1}>Countries PI</h1>
+      <video className={style.video} poster={image} autoPlay muted loop>
+        <source src={video} type="video/mp4"/>
+      </video>
+      <div className={style.container}>
+        <h3 className={style.h3}>Henry PI</h3>
+        <h1 className={style.h1}>Countries App</h1>
         <h2 className={style.h2}>Anderson Marín</h2>
+        <Link to="/home">
+          <button className={style.button}><span>Enter</span></button>
+        </Link>
       </div>
-      <Link to="/home">
-        <button className={style.button}><span>Enter</span></button>
-      </Link>
     </div>
   )
 }
