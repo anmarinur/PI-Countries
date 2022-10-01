2ba3308e869c7a5c16738dfe8ca1f8c23de5a0d4
anmarinur <andersonmarindev@gmail.com>
2022-10-01 13:08:36 -0500

    Add data to Detail component

--- client/src/components/Detail.jsx
+++ client/src/components/Detail.jsx
@@ -11,7 +11,7 @@ export default function Detail(props) {
 
   useEffect(() => {
     dispatch(getCountryId(props.match.params.id));
-  }, [dispatch]);
+  }, [props.match.params.id, dispatch]);
 
   const country = useSelector((state) => state.country);
 
@@ -22,6 +22,35 @@ export default function Detail(props) {
           <div>
             <h1>{country[0].name}</h1>
             <img src={country[0].flagImg} alt="flag" width="250px"/>
+            <div>
+              <p>Id:</p>
+              <h2>{country[0].id}</h2>
+            </div>
+            <div>
+              <p>Capital:</p>
+              <h2>{country[0].capital}</h2>
+            </div>
+            <div>
+              <p>Continent:</p>
+              <h2>{country[0].continent}</h2>
+            </div>
+            <div>
+              <p>Subregion:</p>
+              <h2>{country[0].subregion}</h2>
+            </div>
+            <div>
+              <p>Population:</p>
+              <h2>{country[0].population}</h2>
+            </div>
+            <div>
+              <p>Activities:</p>
+              {
+                country[0].Activities.length > 0 ? country[0].Activities.map ((el) => {
+                    return (<p>{el.name}</p>)
+                  }
+                ) : 'Ninguna actividad'
+              } 
+            </div>
           </div>
         ) : <h1>Loading...</h1>
       }
