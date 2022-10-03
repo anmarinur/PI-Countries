bceba3243d7dcfe24705b3971a72cb38a439c923
anmarinur <andersonmarindev@gmail.com>
2022-10-03 10:31:34 -0500

    Add allow null and remove defaults

--- api/src/models/Activity.js
+++ api/src/models/Activity.js
@@ -10,11 +10,11 @@ module.exports = (sequelize) => {
     },
     name: {
       type: DataTypes.STRING,
-      allowNull: false,
-      defaultValue: ' '
+      allowNull: false
     },
     difficulty: {
       type: DataTypes.INTEGER,
+      allowNull: false,
       validate: {
         min: 1,
         max: 5
@@ -22,10 +22,12 @@ module.exports = (sequelize) => {
     },
     duration: {
       type: DataTypes.INTEGER,
+      allowNull: false,
       defaultValue: 0
     },
     season: {
       type: DataTypes.ENUM('Verano', 'Otoño', 'Invierno', 'Primavera'),
+      allowNull: false
     }
   });
 }
